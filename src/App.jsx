95399ecc0bfa8e1b990ae1e901438c4ec91aa5b3
Debajoti Chakraborty <chakrabortydebajoti@gmail.com>
2024-06-28 23:33:42 +0530

    p1

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,46 @@
 import './App.css'
+import {
+  Brain,
+  CalendarHeartIcon,
+  LineChart,
+  PenBox,
+  ScanFace,
+} from "lucide-react";
+import "./App.css";
+import Sidebar from "./components/Sidebar";
+import { Link } from "react-router-dom";
 
 function App() {
   return (
     <>
-    <h1>Hello World!</h1>
-    </>
+    <div className="h-screen w-full flex gap-4 p-4">
+    <div className="sidebar w-1/5 h-full flex flex-col gap-10 justify-center border-r-2 px-14 rounded-lg">
+      <div className="flex gap-4">
+        <CalendarHeartIcon size={24} />
+        <p className="font-poppins text-lg">
+          <Link to="/">Todos</Link>
+        </p>
+      </div>
+      <div className="flex gap-4">
+        <PenBox size={24} />
+        <p className="font-poppins text-lg">Summarizer</p>
+      </div>
+      <div className="flex gap-4">
+        <Brain size={24} />
+        <p className="font-poppins text-lg">Focus GPT</p>
+      </div>
+      <div className="flex gap-4">
+        <ScanFace size={24} />
+        <p className="font-poppins text-lg">Deep Focus</p>
+      </div>
+      <div className="flex gap-4">
+        <LineChart size={24} />
+        <p className="font-poppins text-lg">Dashboard</p>
+      </div>
+    </div>
+    <div className="main w-4/5 h-full bg-green-500 rounded-lg">Main</div>
+  </div>
+  </>
   )
 }
 
