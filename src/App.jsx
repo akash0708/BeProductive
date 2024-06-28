fc0721f60156103d69267443b535bc3b3ac90d9e
Akash09 <akashbag12a05@gmail.com>
2024-06-28 22:43:28 +0530

    sidebar and main

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
-import './App.css'
+import "./App.css";
+import Sidebar from "./components/Sidebar";
 
 function App() {
-
   return (
-    <>
-    <h1>Hello World!</h1>
-    </>
-  )
+    <div className="h-screen w-full flex gap-4 p-4">
+      <div className="sidebar w-1/4 h-full flex flex-col gap-4 items-center justify-center bg-green-500">
+        <div>Todos</div>
+        <div>Summarizer</div>
+        <div>GPT</div>
+        <div>Deep Focus</div>
+        <div>Dashboard</div>
+      </div>
+      <div className="main w-3/4 h-full bg-green-500">Main</div>
+    </div>
+  );
 }
 
-export default App
+export default App;
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,9 +1,10 @@
 const Sidebar = () => {
   return (
-    <div className="sidebar">
-        
+    <div className="sidebar p-4 flex gap-4 bg-green-500">
+      <div className="h-screen w-1/4 flex">hello</div>
+      <div className="h-screen w-3/4">hello</div>
     </div>
-  )
-}
+  );
+};
 
-export default Sidebar
+export default Sidebar;
